9e68c33becd066ae195cb6943ffcc15ee6d44e11
ParthivPadariya <parthiv.padariya@gmail..com>
2024-01-29 17:12:43 +0530

    textarea

--- src/app/signup/Signup.jsx
+++ src/app/signup/Signup.jsx
@@ -91,10 +91,9 @@ const Signup = () => {
                 <label htmlFor="user_about" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">About</label>
                 <textarea rows={5} 
                 onChange={(e) => setUser({...user, about:e.target.value})}
-                value={user.about}
                 type="text" 
                 defaultValue={user.about}
-                name="user_about" id="user_about" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="About" required />
+                name="user_about" id="user_about" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
             </div>
 
             <div className='mt-3 flex justify-around'>
